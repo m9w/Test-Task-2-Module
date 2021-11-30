866e13c96764273cd44d307c4d09bd61e02e6571
Maksym Natalukha <maksym.natalukha@netcracker.com>
2021-11-30 10:27:11 +0200

    Add tests for 'calculateDiscount'

--- src/test/java/com/example/test/ShoppingCartTest.java
+++ src/test/java/com/example/test/ShoppingCartTest.java
@@ -1,9 +1,11 @@
 package com.example.test;
 
+import com.example.ShoppingCart;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.example.ShoppingCart.ItemType.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShoppingCartTest {
@@ -22,5 +24,11 @@ class ShoppingCartTest {
 
     @Test
     void calculateDiscount() {
+        for (int i = 0; i < 10000; i++) {
+            assertEquals(0, ShoppingCart.calculateDiscount(NEW, i));
+            assertEquals(Math.min(i / 10, 80), ShoppingCart.calculateDiscount(REGULAR, i));
+            assertEquals(i < 2 ? 0 : Math.min((i+500) / 10, 80), ShoppingCart.calculateDiscount(SECOND_FREE, i));
+            assertEquals(Math.min((i+700) / 10, 80), ShoppingCart.calculateDiscount(SALE, i));
+        }
     }
 }
