16d7faa804dc23fdf25ccc82fb9c0d83672fca25
Maksym Natalukha <maksym.natalukha@netcracker.com>
2021-11-30 10:07:49 +0200

    Basic structure

---
+++ src/test/java/com/example/test/ShoppingCartTest.java
@@ -0,0 +1,26 @@
+package com.example.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShoppingCartTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void appendFormatted() {
+    }
+
+    @Test
+    void calculateDiscount() {
+    }
+}
